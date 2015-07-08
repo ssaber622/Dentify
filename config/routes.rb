f4d5ee770f64a40032d8ebab9e78db342b806607
Shayan Saber <shayan.saber@gmail.com>
2015-07-08 15:30:44 -0400

    Added bottom navbar and join us page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root "pages#home" # creates root_path
   get "about" => "pages#about" # creates about_path
-
+  get "joinus" => "pages#joinus"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
